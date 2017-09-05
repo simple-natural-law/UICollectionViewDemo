b0ded93bf0e1f6d0d6fff18138c50cc4041d75de
张诗健 <zsjcwy_xcode@163.com>
2017-09-05 09:47:44 +0800

    create project

---
+++ UICollectionViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UICollectionViewDemo
+//
+//  Created by 讯心科技 on 2017/9/5.
+//  Copyright © 2017年 讯心科技. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
