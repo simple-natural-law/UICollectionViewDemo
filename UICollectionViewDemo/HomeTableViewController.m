c4df414f055689ea6e7c346aa9a14a1e97ce056e
张诗健 <zsjcwy_xcode@163.com>
2017-09-13 10:46:00 +0800

    add

--- UICollectionViewDemo/HomeTableViewController.m
+++ UICollectionViewDemo/HomeTableViewController.m
@@ -30,7 +30,7 @@
     self.view.backgroundColor = [UIColor colorWithRed:0.95 green:0.95 blue:0.95 alpha:1.0];
     
     self.dataSource = @[@{@"title":@"实现一个简单的Collection View",@"target":@"BaseDemoViewController"},
-                        @{@"title":@"可编辑的Collection View",@"target":@"EditableViewController"}];
+                        @{@"title":@"可多选的Collection View",@"target":@"MultipleSelectedViewController"}];
 }
 
 - (void)didReceiveMemoryWarning {
