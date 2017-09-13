0f26d8775643ac2c770baa09438766b6f55cb9c9
张诗健 <zsjcwy_xcode@163.com>
2017-09-13 10:42:51 +0800

    add

---
+++ UICollectionViewDemo/可多选的UICollectionView/MultipleSelectedViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditableViewController.h
+//  UICollectionViewDemo
+//
+//  Created by 讯心科技 on 2017/9/12.
+//  Copyright © 2017年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultipleSelectedViewController : UIViewController
+
+@end
