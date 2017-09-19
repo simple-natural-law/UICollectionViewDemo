1e1dcde1420b8d64d79406293e0474fa335aa85f
zhangshijian <zsjcwy_xcode@163.com>
2017-09-19 23:01:20 +0800

    ad

---
+++ UICollectionViewDemo/不规则瀑布流布局/WaterfallsFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  WaterfallsFlowLayout.h
+//  UICollectionViewDemo
+//
+//  Created by 张诗健 on 2017/9/18.
+//  Copyright © 2017年 讯心科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaterfallsFlowLayout : UICollectionViewFlowLayout
+
+@end
