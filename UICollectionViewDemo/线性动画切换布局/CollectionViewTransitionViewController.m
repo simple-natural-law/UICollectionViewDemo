24d413e6cfeb83630eafd1a013ac392304e6d471
张诗健 <zsjcwy_xcode@163.com>
2017-10-10 18:06:02 +0800

    aa

---
+++ UICollectionViewDemo/线性动画切换布局/CollectionViewTransitionViewController.m
@@ -0,0 +1,41 @@
+//
+//  CollectionViewTransitionViewController.m
+//  UICollectionViewDemo
+//
+//  Created by 讯心科技 on 2017/10/10.
+//  Copyright © 2017年 讯心科技. All rights reserved.
+//
+
+#import "CollectionViewTransitionViewController.h"
+
+@interface CollectionViewTransitionViewController ()
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayoutA;
+
+@end
+
+@implementation CollectionViewTransitionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
